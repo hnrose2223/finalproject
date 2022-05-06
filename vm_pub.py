5cb1aa2fde3e688a4812010a74a4bd0280403c8b
hnrose2223 <hnrose@usc.edu>
2022-05-06 15:51:27 -0700

    Update vm_pub.py

--- vm_pub.py
+++ vm_pub.py
@@ -60,8 +60,70 @@ def FFT():
 	i = 0 #TO DO incorperate FFT page
 	
 	#client.publish("audIOT/FFT", #publish frequency stuff for LED
+	print("Importing {}".format(file)) # file is the mp3 file
+    	audio = AudioSegment.from_mp3(file)
 
+    	sample_count = audio.frame_count()
+    	sample_rate = audio.frame_rate
+    	samples = audio.get_array_of_samples()
+	period = 1/sample_rate                     #the period of each sample
 
+    	slice_sample_size = int(SLICE_SIZE*sample_rate)   #get the number of elements expected for [SLICE_SIZE] seconds
+
+   	n = slice_sample_size                            #n is the number of elements in the slice
+
+   	#generating the frequency spectrum
+    	k = np.arange(n)                                #k is an array from 0 to [n] with a step of 1
+    	slice_duration = n/sample_rate                   #slice_duration is the length of time the sample slice is (seconds)
+    	frq = k/slice_duration                          #generate the frequencies by dividing every element of k by slice_duration
+    
+    	max_frq_idx = int(MAX_FRQ*slice_duration)       #get the index of the maximum frequency (2000)
+    	frq = frq[range(max_frq_idx)]                   #truncate the frequency array so it goes from 0 to 2000 Hz
+    
+    	start_index = 0                                 #set the starting index at 0
+    	end_index = start_index + slice_sample_size      #find the ending index for the slice
+    	output = ''
+   
+   	print()
+    	i = 1
+   	 
+    	while end_index < len(samples):
+        
+        	#print("Sample {}:".format(i))
+        	i += 1
+    
+       		#TODO: grab the sample slice and perform FFT on it
+        	sample_slice = samples[start_index: end_index]
+        	sample_slice_fft = np.fft.fft(sample_slice)/n   #perform fourier transform on sample_slice & normalize by n
+        
+        	#TODO: truncate the FFT to 0 to 2000 Hz
+        	fft = sample_slice_fft[range(max_frq_idx)]
+        
+        	#TODO: calculate the locations of the upper and lower FFT peak using get_peak_frqs()
+        	lower_peak,upper_peak = get_peak_frqs(frq,fft)
+        	print("lower: ", lower_peak)
+        	print("higheer: ",upper_peak)
+        
+        	if (lower_peak < 40): # determine a value to separate high frequencies from low frequencies and blink an LED
+           	 #print("right LED")
+            		#digitalWrite(led_right,1)		# Send HIGH to switch on LED
+			client.publish("audIOT/FFT","LED_ON")
+            		#time.sleep(0.1)
+            		#digitalWrite(led_right,0)
+            
+        	if (upper_peak > 200):
+           	 #print("left LED")
+            		#digitalWrite(led_left,1)		# Send HIGH to switch on LED
+			client.publish("audIOT/FFT","LED_ON")
+            		#time.sleep(0.1)
+            		#digitalWrite(led_left,0)
+        
+        	#Incrementing the start and end window for FFT analysis
+        	start_index += int(WINDOW_SIZE*sample_rate)
+        	end_index = start_index + slice_sample_size
+
+    	print("Program completed")
+    	print("Decoded input: " + str(output))
 
 if __name__ == '__main__':
 	#this section is covered in publisher_and_subscriber_example.py
